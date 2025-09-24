9d3bf17eb83ffb4660df75898933d8e2eda94215
BioHazard <3482991796@qq.com>
2025-09-24 18:42:14 +0800

    fix: 修复 escape_description 函数的文档字符串并添加查找标签的辅助函数

--- arknights_game_model/utils.py
+++ arknights_game_model/utils.py
@@ -1,11 +1,22 @@
 import re
 from itertools import accumulate
+from collections.abc import Iterable
 
 
-def escape_description(s):
-    """删除所有的HTML标签"""
+def escape_description(s: str) -> str:
+    """删除所有的自定义标签"""
     return re.sub(r"<[^>]*?>", "", s)
 
 
-def 计算累计消耗(干员升级消耗: list[list[int]]) -> list[list[int]]:
+def find_dollar_tags(s: str) -> list[str]:
+    """查找所有的 <$...> 标签"""
+    return re.findall(r"<\$(.+?)>", s)
+
+
+def find_at_tags(s: str) -> list[str]:
+    """查找所有的 <@...> 标签"""
+    return re.findall(r"<@(.+?)>", s)
+
+
+def 计算累计消耗(干员升级消耗: Iterable[Iterable[int]]) -> list[list[int]]:
     return [list(accumulate(filter(lambda x: x > 0, 升级消耗列表), initial=0)) for 升级消耗列表 in 干员升级消耗]
