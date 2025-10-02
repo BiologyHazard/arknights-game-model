470913a336f347d266d24ba131b46e18a4dafcfe
BioHazard <3482991796@qq.com>
2025-10-02 21:58:16 +0800

    feat: 在 interactive.py 中新增 UTC 和 CST 时区定义

--- interactive.py
+++ interactive.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import UTC, date, datetime, time, timedelta, timezone
 from pathlib import Path
 
 from arknights_game_model._raw_game_data.game_data import ArknightsGameData, load_data
@@ -6,6 +7,9 @@ from arknights_game_model.building_model import WorkshopFormula
 from arknights_game_model.character_model import Professions, UniEquip, 职业ID_to_职业
 from arknights_game_model.game_data import CharacterDict, GameData, ItemDict, game_data
 from arknights_game_model.item_info_model import ItemBundle, ItemInfo, ItemInfoList
+
+CST = timezone(timedelta(hours=8))
+fts = datetime.fromtimestamp
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--gamedata-folder", type=Path, required=True, help="游戏数据文件夹路径")
